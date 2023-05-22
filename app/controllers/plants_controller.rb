47605b1e1edb605adf1b9bf7e18e71aa16e06e4f
support <support@kmcintosh-6782.remax.local>
2023-05-21 20:41:18 -0600

    commit tree 0c637d4c61009ecfd38bfd5d8ef6a81a31d9b9bf

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -12,6 +12,29 @@ class PlantsController < ApplicationController
     render json: plant
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant 
+      plant.update(plant_params)
+      render json: plant 
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  # DESTROY /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+
   # POST /plants
   def create
     plant = Plant.create(plant_params)
